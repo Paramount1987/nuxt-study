280cd5f96d933edcbe1c4e2c802098c8a67cbd30
Artem Petrov <Paramount7891@gmail.com>
2018-12-17 10:51:51 +0300

    validating parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>A single user, with ID {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate({ params, query }) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
