5786eeabea7473f1f75017c1ddbbfc408c20cf68
Artem Petrov <Paramount7891@gmail.com>
2018-12-19 11:49:30 +0300

    Creating the main 'sections' (pages)

---
+++ pages/about/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about-page">
+    <h1>This is About Page!</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Consequatur impedit ea recusandae perspiciatis doloribus,
+      sed velit assumenda quod non libero maxime eligendi distinctio
+      illum provident sit? Error porro dicta ipsam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Consequatur impedit ea recusandae perspiciatis doloribus,
+      sed velit assumenda quod non libero maxime eligendi distinctio
+      illum provident sit? Error porro dicta ipsam!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  padding: 30px;
+  text-align: center;
+}
+</style>
