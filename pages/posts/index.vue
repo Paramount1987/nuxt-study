aa8696994adb3ce8b1b4ef8b4644db6374a6ec36
Artem Petrov <Paramount7891@gmail.com>
2018-12-23 21:22:27 +0300

    handling errors with a callback

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
@@ -10,6 +10,29 @@ import PostList from '@/components/Posts/PostList'
 export default {
   components: {
     PostList
+  },
+    asyncData(context, callback) {
+    console.log('asyncData context:', context);
+    setTimeout(() => {
+      callback(new Error(), {
+        loadedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This is our first post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          },
+          {
+            id: "2",
+            title: "Second Post",
+            previewText: "This is our second post!",
+            thumbnail:
+              "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+          }
+        ]
+      });
+    }, 1000);
   }
 }
 </script>
